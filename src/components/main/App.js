f6028c770b7266e88e3acf68604883d1137adf30
patricio.filice@gmail.com <patricio.filice@gmail.com>
2022-04-03 18:36:21 -0300

    Changed Favicom. Added possible logos por navbar. Created Header with navbar. Added files for Home and Footer. Installed material-ui. Installed file loader. Installed js-cookie for future usage.

---
+++ src/components/main/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header'
+import Home from './Home'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <main>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </main>
+  );
+}
+
+export default App;
